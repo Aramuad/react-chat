64ca3a8bb6b3005a15b4ad28fff9645e3447d8b7
Aramuad <samcballard@gmail.com>
2018-09-27 11:52:38 -0600

    fix comments

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -3,6 +3,18 @@ import {
   Segment,
   Divider,
 } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+const MyMessage = styled(Segment)`
+  background-color: blue;
+  color: white !important;
+`
+
+const TheirMessage = styled(Segment)`
+  background-color: light-grey;
+`
+
 
 const ChatMessage = ({ body, email, = 'anon' }) => (
   <Segment>
